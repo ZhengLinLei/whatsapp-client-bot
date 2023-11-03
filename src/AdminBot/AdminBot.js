584bc46fcd941e23aff09b480cac7260e46c9541
ZhengLinLei <zheng9112003@gmail.com>
2023-11-03 10:19:24 +0100

    Added timer fetch

--- src/AdminBot/AdminBot.js
+++ src/AdminBot/AdminBot.js
@@ -194,5 +194,17 @@ BOT.addTask('listen', (input, output) => {
     }
 });
 
+BOT.addTask('timer', (input, output) => {
+    if (input.length >= 3) {
+        let t = input.slice(2).join(' ');
+        let i = parseInt(input[1]);
+
+        setTimeout(() => {
+            // Send
+            output(t);
+        }, i * 1000);
+    }
+});
+
 
 BOT.run();
---
+++ src/AdminBot/tasks/timer.js
@@ -0,0 +1,11 @@
+BOT.addTask('timer', (input, output) => {
+    if (input.length >= 3) {
+        let t = input.slice(2).join(' ');
+        let i = parseInt(input[1]);
+
+        setTimeout(() => {
+            // Send
+            output(t);
+        }, i * 1000);
+    }
+});
